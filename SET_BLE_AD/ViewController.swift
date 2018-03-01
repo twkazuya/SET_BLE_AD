307ed0243ad94741ce0a49ff8ab7542bc08cb470
twkazuya <twkazuya@twkazuyademacbook-pro.local>
2018-03-01 14:22:50 +0800

    Initial Commit

---
+++ SET_BLE_AD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SET_BLE_AD
+//
+//  Created by twkazuya on 2018/3/1.
+//  Copyright © 2018年 ANXELL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
